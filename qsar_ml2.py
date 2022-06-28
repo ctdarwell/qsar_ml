37f50585200953b90322041c6cce46ff78c20910
evoclive <ctdarwell@gmail.com>
2022-06-29 06:45:25 +0700

    trivial update

--- qsar_ml2.py
+++ qsar_ml2.py
@@ -72,7 +72,7 @@ ax.set(xlim=(0, 1))
 plt.savefig('matthews_corrcoef.pdf')
 
 
-#Random Forest - looks most consistent across two metrics
+#Random Forest - looks consistently best across two metrics
 # Build random forest model
 from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(n_estimators=500)
